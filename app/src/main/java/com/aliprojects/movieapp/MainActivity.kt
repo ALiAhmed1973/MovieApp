f5c25c4ecabd8c63cc662896028e368f5526d327
AliAhmed1973 <eng_aliahmed1973@yahoo.com>
2021-09-12 16:36:37 +0200

    Create movie_details_fragment

--- app/src/main/java/com/aliprojects/movieapp/MainActivity.kt
+++ app/src/main/java/com/aliprojects/movieapp/MainActivity.kt
@@ -2,13 +2,25 @@ package com.aliprojects.movieapp
 
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.AppBarConfiguration
 import com.aliprojects.movieapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
     lateinit var binding: ActivityMainBinding
+    lateinit var navController: NavController
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
+        val navHost = supportFragmentManager.findFragmentById(R.id.nav_host_fragment_container) as NavHostFragment
+        navController= navHost.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
         setContentView(binding.root)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
     }
 }
